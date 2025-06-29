462f7fcb6150d2c5142343e96e5cf3a3868c69a6
Rushi3223 <rushimistry3223@gmail.com>
2025-06-30 03:02:09 +0530

    3

--- GET_POST.php
+++ GET_POST.php
@@ -1,7 +1,3 @@
-<?php
-
-?>
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -21,7 +17,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="/PHP_TUT/GET_POST.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
@@ -49,17 +45,31 @@
   </div>
 </nav>
 
+<?php
+     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['pass'];
+   echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Yesss!</strong> You can Fill the Form Successfully -> Your Email is ' . $email . ' and Password is ' . $password . '
+  <br> Please do not share your password with anyone.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>';
+}
+
+
+?>
+
 <div class="container mt-5">
   <h1> Enter your Email & Password Here</h1>
     <form action = "GET_POST.PHP" method = "POST">
   <div class="mb-3">
-    <label for="Email" class="form-label">Email address</label>
+    <label for="email" class="form-label">Email address</label>
     <input type="email" class="form-control" id="Emai" name = "email" aria-describedby="emailHelp">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
     <label for="pass" class="form-label">Password</label>
-    <input type="password" class="form-control" id="pass name = "pass">
+    <input type="password" class="form-control" id="pass" name="pass">
   </div>
   
   <button type="submit" class="btn btn-primary">Submit</button>
@@ -69,5 +79,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
   </body>
 </html>
-   
- 
+
